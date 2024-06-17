57b7733298de6030cdfacc0d529296122aac147d
Percival <emperorsheevpalpatine0@gmail.com>
2024-06-17 12:40:08 +0800

    feat(BE): add oceanopedia-article module

---
+++ oceanopedia-article/src/main/java/com/oceanopedia/dao/ArticleDao.java
@@ -0,0 +1,27 @@
+package com.oceanopedia.dao;
+
+
+
+import com.oceanopedia.entity.Article;
+
+import java.util.List;
+
+public interface ArticleDao {
+    Article findArticleById(String id);
+
+    List<Article> findArticlesByCategory(String category, int page, int rows);
+
+    void deleteArticleByID(String id);
+
+    int countArticleByCategory(String category);
+
+    void saveArticle(Article Article);
+
+    void updateArticleStatusById(String id, String status);
+
+    void updateArticleInfoById(String id, Article Article);
+
+    void updateArticleCoverImageById(String id, String coverImage);
+
+
+}
